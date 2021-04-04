621bdd2e7cd5dd791fd8ad63f0cfb904150b875f
Michael Arvelo <michaeldevelopscode@gmail.com>
2021-04-04 16:14:08 -0400

    post built

---
+++ client/build/precache-manifest.0a4487bd02123a42615dc6a8a5532fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e420539c52fe54696dbcffee701cd2e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f595f7794371e2d508eb",
+    "url": "/static/css/main.fcddab7e.chunk.css"
+  },
+  {
+    "revision": "e71030ce65df44ac3a5f",
+    "url": "/static/js/2.af0a1ea7.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.af0a1ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f595f7794371e2d508eb",
+    "url": "/static/js/main.d22f74a1.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
